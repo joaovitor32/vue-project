b645b880b15686701ac186d717dfcc009278aac7
joaovitor32 <joaovitormunizlopes@gmail.com>
2021-07-07 23:58:19 -0300

    Updating main page

--- src/pages/Main/Main.vue
+++ src/pages/Main/Main.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
     <p style="font-size: 120px">Main page</p>
-    <p style="font-size: 120px">Main page</p>
-    <p style="font-size: 120px">Main page</p>
-    <p style="font-size: 120px">Main page</p>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { defineComponent } from "vue";
 
-export default class Main extends Vue {}
+import api from "../../shared/service/axios";
+
+interface Project {
+  id: number;
+  name: string;
+  description: string;
+  url: string;
+}
+
+export default defineComponent({
+  name: "MainPage",
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    this.load();
+  },
+  methods: {
+    async load() {
+      try {
+        const response = await api.get<Project[]>("/projects");
+        this.$data.projects = response.data;
+      } catch (err) {
+        throw Error(err);
+      }
+    },
+  },
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
