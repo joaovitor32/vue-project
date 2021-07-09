281908bf9ac6f73c6a075be83438c98ee09ba0bd
joaovitor32 <joaovitormunizlopes@gmail.com>
2021-07-08 23:27:44 -0300

    Adding project item component

---
+++ src/pages/Main/components/ProjectItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="projectItem">
+    <p>{{ projectItem.name }}</p>
+    <p>{{ projectItem.description }}</p>
+  </div>
+  <FooterComponent />
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+interface Project {
+  id: number;
+  name: string;
+  description: string;
+  url: string;
+}
+
+export default defineComponent({
+  name: "ProjectItem",
+  props: {
+    projectItem: {
+      type: Object as PropType<Project>,
+      default(this: void, project: Project) {
+        return project;
+      },
+      validator(this: void, project: Project) {
+        return !!project;
+      },
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
