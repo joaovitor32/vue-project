3f0c8c211de0585a6785af11f7abb5e8c9a34c1b
joaovitor32 <joaovitormunizlopes@gmail.com>
2021-07-11 22:49:41 -0300

    Adding light theme

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,6 +1,11 @@
+export const light = {
+  primary: "#326e6e",
+  secondary: "#c1c7df",
+  terciary: "#fefefe",
+};
+
 export const dark = {
   primary: "#16151a",
   secondary: "#ed8e2a",
-  terciary: "#fefefe",
-  anchor: "#8c9eff",
+  terciary: "#7c99bb",
 };
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,35 @@
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/lib/styles/main.sass'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/lib/components'
-import * as directives from 'vuetify/lib/directives'
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/lib/components";
+import * as directives from "vuetify/lib/directives";
+
+import "vuetify/dist/vuetify.min.css";
+
+import { dark, light } from "../config/colors";
 
 export default createVuetify({
   components,
   directives,
-})
+  treeShake: true,
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    defaultTheme: "light",
+    themes: {
+      light: {
+        colors: {
+          primary: light.primary,
+          secondary: light.secondary,
+          terciary: light.terciary,
+        },
+      },
+      dark: {
+        colors: {
+          primary: dark.primary,
+          secondary: dark.secondary,
+          terciary: dark.terciary,
+        },
+      },
+    },
+  },
+});
